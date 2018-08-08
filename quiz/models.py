8ed30281b3014b725fef588581070e7345b24d7c
Daniela Kreimerman Arroyo <danikreimerman@gmail.com>
2018-08-08 20:56:26 +0800

    messed around with models some

--- quiz/models.py
+++ quiz/models.py
@@ -4,8 +4,8 @@ class Quiz(models.Model):
     ''' 
     This is the model for a single quiz/questionnaire.
     '''
-    title = models.CharField(max_length=100)
-    desc = models.TextField()
+    title = models.CharField(max_length=100,default='')
+    desc = models.TextField(default='')
 
     def __str__(self):
         return self.title
@@ -16,8 +16,8 @@ class Profile(models.Model):
     "Depression" would be a profile, and the quiz would tally how many points the user scored
     that fall under the "Depression" profile. 
     '''
-    name = models.CharField(max_length=100)
-    desc = models.TextField()
+    name = models.CharField(max_length=100,default='')
+    desc = models.TextField(default='')
     quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
     tally = models.IntegerField(default=0)
 
@@ -34,6 +34,30 @@ class Question(models.Model):
 
     def __str__(self):
         return self.txt
+'''
+    def make_scale(self,labels=['Not at all','A little','Moderately','Quite a bit','Extremely']):
+        idx = 0
+        for lb in labels:
+            self.choice_set.create(txt=label, 
+                                 points= idx,
+                                 question = self)
+            idx+=1
+'''
+
+class ScaleQuestion(Question):
+''' WIP --- Making a Question whose choices go from Not at all (0 points) to Extremely (4 pts)
+'''
+    def setup(self):
+        SCALE = [('Not at all', 0),
+             ('A little', 1),
+             ('Moderately', 2),
+             ('Quite a bit', 3),
+             ('Extremely', 4)]
+        for opt in SCALE:
+            choice_set.create(txt=opt[1],points=opt[2],question=self)
+
+    class Meta:
+        proxy = True
 
 class Choice(models.Model):
     '''

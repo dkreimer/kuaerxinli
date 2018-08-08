775ea2fc7d70398333c09c8c4f0baf43a2f540ab
Daniela Kreimerman Arroyo <danikreimerman@gmail.com>
2018-08-09 01:29:19 +0800

    trying to figure out submission of the form.... having a hard time plugging things together. its 1am.

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('quiz.urls'),name='index'),
+    #path('',include('quiz.urls'),name='index'),
     path('quiz/',include('quiz.urls')),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,14 +1,52 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Quiz
+from .models import Quiz, Question, Choice, Profile
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 def take(request,quiz_id):
     return render(request, 'quiz/take.html', {'quiz':get_object_or_404(Quiz,pk=quiz_id)})
 
 def submit(request,quiz_id):
-    return render(request,'quiz/submit.html',{})
+    quiz = get_object_or_404(Quiz,pk=quiz_id)
+    for question in quiz.question_set.all():
+        p = question.profile
+        print (request.POST)
+        #try:
+        #selected = question.choice_set.get(pk=request.POST['choice-q'+str(question.id)])
+        #p.tally += selected.points
+        #p.save()
+        # except (KeyError, Choice.DoesNotExist):
+        #     # Redisplay voting form
+        #     return render(request,'quiz/take.html',{
+        #         'quiz': quiz, 
+        #         'error_message': 'You didn\'t select a choice'
+        #         })
+    return HttpResponseRedirect(reverse('quiz:results',args=(question.id)))
+
+    
+
+'''
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+'''
 
 def results(request,quiz_id):
-    return render(request,'quiz/results.html',{})
+    return render(request,'quiz/results.html',{quiz: get_object_or_404(Quiz,pk=quiz_id)})
 
 def index(request):
     return render(request,'quiz/index.html',{'quizzes':Quiz.objects.all()})

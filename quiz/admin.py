95f2756697c04688515f3731cfd7cc30ee1adcff
Daniela Kreimerman Arroyo <danikreimerman@gmail.com>
2018-08-17 18:16:58 +0800

    reworked some models + add quizzes and questions from admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quiz, Question, Choice, Profile
+
+class ChoicesInline(admin.TabularInline):
+    model = Choice.questions.through
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoicesInline]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    inlines = [ChoicesInline]
+    exclude = ('choices',)
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    extra = 1
+
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 1
+
+class QuizAdmin(admin.ModelAdmin):
+    radio_fields = {"status": True}
+    inlines = [ProfileInLine,QuestionInLine]
+
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 class Quiz(models.Model):
     ''' 
     This is the model for a single quiz/questionnaire.
     '''
+    STATUS_CHOICES = (
+		(1, ('Draft')),
+		(2, ('Public')),
+		(3, ('Close')),
+)
     title = models.CharField(max_length=100,default='')
     desc = models.TextField(default='')
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+    status = models.IntegerField(choices = STATUS_CHOICES, default= 1)
 
     def __str__(self):
         return self.title
@@ -18,7 +26,7 @@ class Profile(models.Model):
     '''
     name = models.CharField(max_length=100,default='')
     desc = models.TextField(default='')
-    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz,on_delete=models.CASCADE, blank = True, null=True)
     tally = models.IntegerField(default=0)
 
     def __str__(self):
@@ -28,9 +36,12 @@ class Question(models.Model):
     '''
     Model for a question.
     '''
+    #QUESTION_TYPES = (('Scale','Scale'))
+
     txt = models.CharField(max_length=200)
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, blank = True, null = True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank= True, null=True)
+    #Qtype = models.CharField(choices = QUESTION_TYPES, default = 'Scale')
 
     def __str__(self):
         return self.txt
@@ -65,7 +76,7 @@ class Choice(models.Model):
     '''
     txt = models.CharField(max_length=100)
     points = models.IntegerField()
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    questions = models.ManyToManyField(Question, blank = True, related_name= 'choice')
 
     def __str__(self):
         return self.txt
